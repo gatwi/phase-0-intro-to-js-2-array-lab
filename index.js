da1c10acfa99da4ca16de36736b1d645126db012
gatwirimurithi <faith.murithi@student.moringaschool.com>
2022-06-04 01:53:56 +0300

    commit to master

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution here!
+var cats = ['Milo','Otis','Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name);
+    return cats;
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift(name);
+    return cats;
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+    return cats
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name){
+    var name_1=[...cats,name];
+    return name_1;
+}
+
+function prependCat(name){
+    var name_2=[name, ...cats];
+    return name_2;
+}
+
